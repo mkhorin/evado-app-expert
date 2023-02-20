908ab8fff2799874c039b69cc85650d7d41728a8
mkhorin <maksimovichu@gmail.com>
2023-02-20 10:20:05 +1000

    Update 5.6.0

--- module/api/base/controller/ExpertController.js
+++ module/api/base/controller/ExpertController.js
@@ -10,7 +10,8 @@ module.exports = class ExpertController extends Base {
     async actionFirstQuestion () {
         const cls = this.getMetadataClass('question');
         const view = this.getMetadataView('publicList', cls);
-        const query = view.createQuery(this.getSpawnConfig());
+        const config = this.getSpawnConfig();
+        const query = view.createQuery(config);
         const counter = await query.count();
         if (!counter) {
             throw new NotFound('First question no found');
@@ -31,50 +32,55 @@ module.exports = class ExpertController extends Base {
         const answerClass = this.baseMeta.getClass('answer');
         const questionClass = this.baseMeta.getClass('question');
         const entityClass = this.baseMeta.getClass('entity');
-        const answers = await answerClass.findById(answerIds).raw().all();
+        const answerQuery = answerClass.findById(answerIds).raw();
+        const answers = await answerQuery.all();
         const includedAttrs = this.getAnswerAttrs(answers, false);
         const excludedAttrs = this.getAnswerAttrs(answers, true);
         const condition = includedAttrs.map(id => ({attributes: id}));
         if (excludedAttrs.length) {
             condition.push(['notIn', 'attributes', excludedAttrs]);
         }
-        const entities = await entityClass.find(...condition).raw().all();
+        const entityQuery = entityClass.find(...condition).raw();
+        const entities = await entityQuery.all();
         if (!entities.length) {
             return this.sendJson({message: 'No matching entities'});
         }
         if (entities.length === 1) {
             return this.sendResult(entities, entityClass);
         }
-        const entityAttrs = [].concat(...ArrayHelper.getPropertyValues('attributes', entities));
+        const entityValues = ArrayHelper.getPropertyValues('attributes', entities);
+        const entityAttrs = [].concat(...entityValues);
         const newAttrs = MongoHelper.diff(entityAttrs, includedAttrs);
         if (!newAttrs.length) {
             return this.sendResult(entities, entityClass);
         }
         const newAnswers = await answerClass.find({attributes: newAttrs}).ids();
-        const newQuestions = await questionClass.find()
+        const questionQuery = questionClass.find()
             .and({active: true, answers: newAnswers})
-            .and(['notId', questionClass.getKey(), questionIds]).ids();
+            .and(['notId', questionClass.getKey(), questionIds]);
+        const newQuestions = await questionQuery.ids();
         if (!newQuestions.length) {
             return this.sendResult(entities, entityClass);
         }
         const id = ArrayHelper.random(newQuestions);
         const view = questionClass.getView('publicList');
         const model = await view.findById(id).withReadData().one();
-        this.sendJson({question: model.output()});
+        const question = model.output();
+        this.sendJson({question});
     }
 
     getAnswerAttrs (answers, except) {
         answers = answers.filter(data => data.except === except);
-        return [].concat(...ArrayHelper.getPropertyValues('attributes', answers))
+        const values = ArrayHelper.getPropertyValues('attributes', answers);
+        return [].concat(...values);
     }
 
     async sendResult (entities, entityClass) {
         const ids = entities.map(data => data[entityClass.getKey()]);
         const view = entityClass.getView('publicList');
         const models = await view.findById(ids).all();
-        this.sendJson({
-            result: models.map(model => model.output())
-        });
+        const result = models.map(model => model.output());
+        this.sendJson({result});
     }
 };
 module.exports.init(module);
